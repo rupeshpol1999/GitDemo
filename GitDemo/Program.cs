ad434501bfcfc28a5ebfaed453061ce0c0073f98
rupeshpol1999 <rupeshpol1999@gmail.com>
2025-06-08 09:17:19 +0530

    Arithmatic operations Add

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("First change of my repo");
+
+int a, b;
+string operation = "";
+
+Console.Write("Enter The first number : ");
+int.TryParse(Console.ReadLine(), out a);
+
+Console.Write("Enter The first number : ");
+int.TryParse(Console.ReadLine(), out b);
+
+Console.Write("Enter +,-,/,*  :  ");
+operation = Console.ReadLine();
+
+switch (operation)
+{
+    case "+":
+        Console.WriteLine($"The Given Number Addition is : {a + b}");
+        break;
+
+    case "-":
+        Console.WriteLine($"The Given Number Substraction is : {a - b}");
+        break;
+
+    case "/":
+        Console.WriteLine($"The Given Number Division is : {a / b}");
+        break;
+
+    case "*":
+        Console.WriteLine($"The Given Number Product is : {a * b}");
+        break;
+    default:
+        Console.WriteLine("Invalid Operation");
+        break;
+}
+
